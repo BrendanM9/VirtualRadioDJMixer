9e80ff962e5bdb735e80686b31c3621a10c8afaf
BrendanM9 <124943530+BrendanM9@users.noreply.github.com>
2023-10-03 21:03:54 -0700

    Add files via upload

--- KQAK.js
+++ KQAK.js
@@ -1,9 +1,15 @@
-const data1 = require('./lib.json');
+/*const data1 = require('./lib.json');
 console.log("data1 /n", data1);
 document.getElementById("Song1").innerHTML = "data1[1].song"
 
 fetch('./lib.json')
 .then(response => {
-  return(response.json();
+  return(response.json());
 })
 .then(data => console.log(data));
+*/
+var songno = .1;
+for(var i = 0; i < 190; i++){
+  songno = Math.floor(Math.random() * 190);
+  console.log(songno)
+}
